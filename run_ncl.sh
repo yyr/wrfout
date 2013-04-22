5aa3786ad5744991282e3d3e0685ad4e859dddc4
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-04-22 16:54:38 +0900

    * run_ncl.sh: clean up

--- run_ncl.sh
+++ run_ncl.sh
@@ -2,13 +2,7 @@
 # Created: Tuesday, January 22 2013
 
 for dir in `cat ./modelout_dir`; do
-    echo $dir
     cd $dir
-    file=`ls wrfout*`
-    file="${file%\\n}"
-
-    export NCL_FIN=$file
-    echo ncl_fin: $NCL_FIN
+    echo RUNNING NCL IN: $dir
     ncl $@                          # eg ./run_ncl.sh foo.ncl
-
 done
