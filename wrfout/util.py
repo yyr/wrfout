32f9471e489b87c6ae1de2b31397e17869d3fe87
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-13 07:45:03 +0900

    * wrfout/util.py: var query functions

--- wrfout/util.py
+++ wrfout/util.py
@@ -15,6 +15,9 @@ def find_inputfile(dom=3):
         else:
             return None
 
+def var_list(fh):
+    return fh.variables.keys()
+
 def print_var_summary(fh,varname=None):
     if varname is not None:
         try:
@@ -25,6 +28,24 @@ def print_var_summary(fh,varname=None):
     else:
         print('No variable is given listing all vars  ' +
               '\n  '.join(fh.variables.keys()))
+
+def var_grouped(fh,by_type="dimension"):
+    if by_type == "dimension":
+        o_d = []
+        t_d = []
+        th_d = []
+        f_d = []
+        for var in var_list(fh):
+            v = fh.variables[var]
+            if v.rank == 1:
+                o_d.append(var)
+            elif v.rank == 2:
+                t_d.append(var)
+            elif v.rank == 3:
+                th_d.append(var)
+            elif v.rank == 4:
+                f_d.append(var)
+    return {'1d' : o_d, '2d' : t_d, '3d' : th_d, '4d' : f_d}
 
 if __name__ == '__main__':
     pass
