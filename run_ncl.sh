bea325c141491e26b05bcccdcd57c4717c3d853d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-23 03:35:24 +0900

    * run_ncl.sh: delete old code

--- run_ncl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Created: Tuesday, January 22 2013
-
-for dir in `cat ./modelout_dir`; do
-    cd $dir
-    echo RUNNING NCL IN: $dir
-    ncl $@                          # eg ./run_ncl.sh foo.ncl
-done
