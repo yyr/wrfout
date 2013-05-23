a13cb75267c7f46e2199c1695a13321acae4d994
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2013-05-24 08:22:11 +0900

    * setup.py: add more classifiers

--- setup.py
+++ setup.py
@@ -14,8 +14,12 @@ setup(
     long_description=wrfout.__doc__,
     keywords='wrf, wrfout, weather',
     classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Intended Audience :: Science/Research",
+        "License :: FSF Approved :: GPL v3 License",
         "Topic :: Scientific/Engineering :: Science",
+        "Classifier: Topic :: Scientific/Engineering :: Visualization"
     ],
 )
